e55c246c5d107e78649c1640c545de0bdd2d58e3
Robert Piatek <mrseiseo@gmail.com>
2018-06-14 11:31:21 +0200

    Adding first class

---
+++ Code/cpp/Administrator.cpp
@@ -0,0 +1,11 @@
+#include "Administrator.h"
+
+void Administrator::UsunKonto() {
+	// TODO - implement Administrator::UsunKonto
+	throw "Not yet implemented";
+}
+
+void Administrator::ZmienUprawnieniaKonta() {
+	// TODO - implement Administrator::ZmienUprawnieniaKonta
+	throw "Not yet implemented";
+}
---
+++ Code/h/Administrator.h
@@ -0,0 +1,13 @@
+#ifndef ADMINISTRATOR_H
+#define ADMINISTRATOR_H
+
+class Administrator : Moderator {
+
+
+public:
+	void UsunKonto();
+
+	void ZmienUprawnieniaKonta();
+};
+
+#endif
