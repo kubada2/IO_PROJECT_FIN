93ca251ff6e96b0b2baf3fbb0a511d2b55cca0ea
Robert Piatek <mrseiseo@gmail.com>
2018-06-14 11:35:09 +0200

    getting project ready

---
+++ Code/cpp/Baza danych.cpp
@@ -0,0 +1,6 @@
+#include "Baza danych.h"
+
+void Baza_danych::Zwroc_odnosnik() {
+	// TODO - implement Baza danych::Zwroc_odnosnik
+	throw "Not yet implemented";
+}
---
+++ Code/h/Baza danych.h
@@ -0,0 +1,11 @@
+#ifndef BAZA DANYCH_H
+#define BAZA DANYCH_H
+
+class Baza_danych {
+
+
+public:
+	void Zwroc_odnosnik();
+};
+
+#endif
