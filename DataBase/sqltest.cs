6ecc6dac4c03ea10301f9e5d1d451cd2fb42a6d0
Robert Piatek <mrseiseo@gmail.com>
2018-06-17 20:38:14 +0200

    adding databaseconnecter

---
+++ DataBase/sqltest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlTest_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          SqlConnection conn = new SqlConnection("Server = localhost ;Database=filmy; Integrated Security = true");
+          conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * from filmy WHERE id_tytulu = 1");
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+            }
+            reader.Close();
+            conn.Close();
+        }
+    }
+}
